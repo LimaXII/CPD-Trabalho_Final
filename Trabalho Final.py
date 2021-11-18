f29579f634d66cb33a8673692ee8fd3d44195b09
Luccas Lima <85250416+LimaXII@users.noreply.github.com>
2021-11-18 20:58:17 -0300

    Arrumei a trie e contabilizei o tempo
    Procura por player funcionando.

--- Trabalho Final.py
+++ Trabalho Final.py
@@ -3,9 +3,10 @@
 #Abre e le o arquivo de nomes.
 #Funcao que cria a tabela hash.    
 
+import time  #Usado para cronometrar o tempo para gerar as estruturas utilizadas no trabalho.
+
 #Classe que ira armazenar a funções do nodo da árvore trie.
 #from types import NoneType
-
 class TrieNode: 
     def __init__(self, char): 
         self.char = char 
@@ -50,48 +51,38 @@ class Trie(object):
 #Função que inserte todos os nomes na árvore trie.
 def insert_trie(contents):
     for x in range (0, len(contents)):          #Percorre o vetor inteiro.
-        test = 0                                #Variável que irá guardar se vai foi passada uma ',' ou não. 
-        string = ''                             #Uma string vazia.
-        for i in contents[x]:                   #Percorre o nodo do vetor.
-            if (i == ','):                      #Caso ache uma ','
-                test += 1                       #Test recebe +1.
-            if (test == 1) and ((i != '\n') and (i != ',')):    #Caso esteja lendo o nome do jogador.        
-                string = string + (i)           #Cria uma string com o nome completo do jogador.                         
-        tr.insert(string)                       #Insere ela na trie.
-
-
-
+        for i in range (0, len(contents[x])):                               
+            tr.insert(contents[x][i][1])        #Insere ela na trie.
 
 def hash_table(contents, M, vet):
     #Variavel que ira guardar o numero correspondente a cada nome    
-    for x in range (0, len(contents)):                  #Percorre todas os nomes da lista.                
-        hash_value = horner_method_players(contents[x][0], M)      #Chama a funcao do metodo de Horner, para achar uma posicao adequada para o nome na tabela hash.                               
+    for x in range (0, len(contents)):                         #Percorre todas os nomes da lista.                
+        hash_value = horner_method_players(contents[x][0], M)  #Chama a funcao do metodo de Horner, para achar uma posicao adequada para o nome na tabela hash.                               
         hash_insert_rating(hash_value, vet,contents[x])        #Chama a funcao que ira inserir os nomes na tabela hash.
 
 def hash_table_players(contents, M, vet):
     #Variavel que ira guardar o numero correspondente a cada nome    
-    for x in range (0, len(contents)):                      #Percorre todas os nomes da lista.                
-        hash_value = horner_method_players(contents[x][0], M)  #Chama a funcao do metodo de Horner, para achar uma posicao adequada para o nome na tabela hash.                               
-        hash_insert(hash_value, vet,contents[x])            #Chama a funcao que ira inserir os nomes na tabela hash.
+    for x in range (0, len(contents)):                             #Percorre todas os nomes da lista.                
+        hash_value = horner_method_players_smp(contents[x][1], M)  #Chama a funcao do metodo de Horner, para achar uma posicao adequada para o nome na tabela hash.                               
+        hash_insert(hash_value, vet,contents[x])                   #Chama a funcao que ira inserir os nomes na tabela hash.
 
 def hash_table_players_in_rating(contents, M, vet):
     #Variavel que ira guardar o numero correspondente a cada nome    
-    for x in range (0, len(contents)):                      #Percorre todas os nomes da lista.                
+    for x in range (0, len(contents)):                         #Percorre todas os nomes da lista.                
         hash_value = horner_method_players(contents[x][0], M)  #Chama a funcao do metodo de Horner, para achar uma posicao adequada para o nome na tabela hash.                               
         hash_insert_names_in_media(hash_value, vet,contents[x][0]) 
 
 def insert_ratings(contents, M, vet):
     #Variavel que ira guardar o numero correspondente a cada nome    
-    for x in range (0, len(contents)):                      #Percorre todas os nomes da lista.                
+    for x in range (0, len(contents)):                         #Percorre todas os nomes da lista.                
         hash_value = horner_method_players(contents[x][1], M)  #Chama a funcao do metodo de Horner, para achar uma posicao adequada para o nome na tabela hash.                               
         hash_insert_ratings_in_media(hash_value, vet,contents[x]) 
-
 
 #Calcula o polinomio de horner para a tabela hash dos JOGADORES.
 def horner_method_players(word, M):
     p = 31
     hash_value = (p * int(word)) % M
-    return(hash_value)      #Retorna a posição do vetor a ser colocado o jogador.
+    return(hash_value)       #Retorna a posição do vetor a ser colocado o jogador.
 
 #Mesma função que a de cima, mas simplificada.
 #Apenas calcula o hash e retorna ele.
@@ -101,21 +92,6 @@ def horner_method_players_smp(word, M):
     for i in word:        
             num = ord(i)
             hash_value = (p * hash_value + num) % M
-    return(hash_value)
-    
-#Funcao que calcula o valor do polinomio especifico, dado um nome.
-def horner_method(word, M):
-    test = 0
-    p = 11                                              #Primeiro numero primo maior que 10.
-    hash_value = 0                                      #Hash comeca valendo 0.
-    sigN = 7
-    for i in word:                                      #Percorre a palavra.
-        if (i == ','):
-            test += 1                                           #Marca que passou pela primeira virgula.
-        if (test == 1) and ((i != '\n') and (i != ',')):        #Testa se o char eh diferente de "\n" e ",".            
-            num = ord(i)                                        #Caso seja diferente, transforma o char para int e acumula em uma soma.
-            hash_value = ((sigN * hash_value) + num) % M        #Cria um polinomio correspondente a palavra.
-            sigN -= 1            
     return(hash_value)
 
 #Insere o nome em uma determinada posicao da tabela.
@@ -139,36 +115,42 @@ def hash_insert_names_in_media(pos, vet, name):
     vet[pos].append([])
     i+=1
     vet[pos][i].append(name)
-    return
-        
+    return        
 
 def hash_insert_rating(pos, vet, name):     
     for i in range(0,len(vet[pos])):
-        if(vet[pos][i][0]==name[0]): #se o usuário já deu rating para outro jogador
-            vet[pos][i].append(name[1]) #apenas dá um append com o ID do jogador e o rating
+        if(vet[pos][i][0]==name[0]):      #se o usuário já deu rating para outro jogador
+            vet[pos][i].append(name[1])   #apenas dá um append com o ID do jogador e o rating
             vet[pos][i].append(name[2])
             return
     vet[pos].append(name)
     return
 
-
-
 #Realiza a consulta na tabela hash dos jogadores.
 def realizar_consulta_players(vet,name):    
-    aux = horner_method_players_smp(name,len(vet))   #Calcula a posição em que está o nome solicitado na tabela hash.
-    for i in range(0,len(vet[aux])):
-        test = 0
-        string = ''                         #Declara uma string vazia, para guardar o nome do jogador.
-        for j in vet[aux][i]:
-            if (j == ','):                  #Caso ache uma ','.
-                test += 1                   #test recebe +1.
-            if (test == 1) and ((j != '\n') and (j != ',')):        #Testa se o char eh diferente de "\n" e ",".
-                string = string + (j)       #Cria uma string da posição atual do nodo no vetor.
-        if(name == string):                 #Caso o nome procurado exista na tabela:         
-            return (vet[aux][i])            #Retorna todas as informações sobre o jogador solicitado.
-    return('Not found.')                    #Caso o jogador não exista.
+    aux = horner_method_players_smp(name,len(vet))  #Calcula a posição em que está o nome solicitado na tabela hash.
+    for i in range(0,len(vet[aux])):                #Declara uma string vazia, para guardar o nome do jogador.                     
+        if(name == vet[aux][i][1]):                 #Caso o nome procurado exista na tabela:  
+            player_id = vet[aux][i][0]  
+            player_name = vet[aux][i][1]
+            string = (player_id + '   ' + player_name + '   ')
+            for x in range (2, len(vet[aux][i])):
+                player_pos = vet[aux][i][x]                
+                string = string + player_pos   
+            string = string.replace("\n", "")         
+            return (string)                         #Retorna todas as informações sobre o jogador solicitado.    
+    return('Not found.')                            #Caso o jogador não exista.
+
+#Função que transforma um vetor de string em uma string normal.
+def transform_to_str(text):
+    str1 = ""
+    for x in text:
+        str1 += x
+    return (str1)
 
 #------------------------------------------------------------------
+start_time = time.time() #Inicia a contagem de tempo.
+
 #Leitura dos arquivos do trabalho.
 with open('players_clean2.csv') as f:   #Usei o arquivo clean2. O arquivo original não tava lendo por conta da acentuação em alguns nomes.
     players = f.readlines()             #Armazena cada campo em uma posição do vetor players.
@@ -176,7 +158,7 @@ for i in range(0,len(players)):
     a=players[i].split(',')
     players[i]=a
 
-#with open('rating.csv') as f:          #Lê o arquivo rating. Arquivo ENORME que consome muito do programa. Precisamos rever isso.
+#with open('rating.csv') as f:          #Lê o arquivo rating.
 #    rating = f.readlines()             #Armazena cada campo em uma posição do vetor rating.
 
 with open('minirating.csv') as f:       #Arquivo minirating. To usando ele só pra não travar o programa inteiro.
@@ -184,8 +166,7 @@ with open('minirating.csv') as f:       #Arquivo minirating. To usando ele só p
 
 for i in range(0,len(rating)):
     a=rating[i].split(',')
-    rating[i]=a        
-
+    rating[i]=a  
 
 with open('tags.csv') as f:             #Lê o arquivo tags.csv. 
     tags = f.readlines()                #Armazena cada campo em um posição do vetor tags.
@@ -198,16 +179,13 @@ del rating[0]                          #Elimina a primeira posição do vetor.
 del tags[0]                            #Elimina a primeira posição do vetor.
 del players[0]                         #Elimina a primeira posição do vetor.
 #------------------------------------------------------------------
-
-#------------------------------------------------------------------
 #N/M = 1/3
-M = int(len(rating)/4)                  #M será o tamanho da tabela Hash a ser criada para armazenar as médias das avaliações e o total de avaliações para cada jogador.
+M = int(len(rating)/4) #M será o tamanho da tabela Hash a ser criada para armazenar as médias das avaliações e o total de avaliações para cada jogador.
 #Rating_vet é uma tabela Hash que vai armazenar em cada nodo, todas as notas de um mesmo jogador.
 #Atualmente anda dando alguns problemas nela, ta armazendo as vezes 2 ou 3 jogadores diferentes no mesmo nodo.
 rating_vet = [[] for _ in range(0,M)]   #Vetor que ira armazenar a tabela hash com as notas de cada jogador.   
 hash_table(rating, M, rating_vet)       #Chama a funcao para criar a tabela hash.
 #------------------------------------------------------------------
-
 #caad valor dos ratings na tabela hash é um array com o primeiro elemento representando
 #o ID do usuário e os elementos seguintes são o ID do jogador que ele deu review e a nota 
 #atribuída, podendo repetir esses elementos mais de uma vez caso o usuário tenha dado review 
@@ -218,8 +196,6 @@ hash_table(rating, M, rating_vet)       #Chama a funcao para criar a tabela hash
 #review para o jogador de ID 220440: 5.0
 #review para o jogador de ID 200529: 3.0
 
-
-
 '''
 for i in range(0,len(rating_vet)):
     if(len(rating_vet[i])>0):
@@ -227,38 +203,47 @@ for i in range(0,len(rating_vet)):
             if(len(rating_vet[i][j])>3):
                 print(rating_vet[i][j])
                 '''
-#------------------------------------------------------------------
-#print(rating_vet)                      #Teste da tabela hash das ratings.
-#------------------------------------------------------------------
 
 #------------------------------------------------------------------
 #Criação da tabela Hash com as informações complementares dos jogadores.
-M = int(len(players)/5)                           #M = len(players), já que não pode ocorrer colisões.
+M = int(len(players)/5)                           
 players_vet = [[] for _ in range(0,M)]            #Vetor que ira armazenar a tabela hash com as informações adicionais de cada jogador.
 hash_table_players(players, M, players_vet)       #Chama a funcao para criar a tabela hash.
 #------------------------------------------------------------------
-
 players_media =[[[]] for _ in range(0,M)]
 hash_table_players_in_rating(players,M,players_media)
 
 insert_ratings(rating,M,players_media)
-
-print(rating_vet[3])
-
-print(players_vet[1])
-print(players_media[422])
 #------------------------------------------------------------------
 #Árvore trie com os nomes dos jogadores.
 tr = Trie()
 #Chama a função que vai inserir na árvore trie todos os nomes dos jogadores.
-insert_trie(players)
-#Apenas testando a funcionalidade da árvore trie.
-name = input("Please insert a name to search.")
-consult = (tr.search(name))         #Pega todos os nomes, dado o prefixo dado
-print("sofifa_id   name   player_positions   rating   count")
-for x in consult:                   #Para cada nome encontrado.
-    info = realizar_consulta_players(players_vet,x)      #Recebe o resto das informações pela tabela hash dos jogadores.    
-    print(info)                     #Printa as informações adicionais.
+insert_trie(players_vet)
+
+#Finaliza a criação de estruturas para o programa.
+end_time = time.time()
+print("Time to create the structures " + '{:.2f}'.format(end_time - start_time) + " seconds.")
+
+running = 1                         #Variável que vai controlar quando o programa irá terminar.
+while (running == 1):               #Loop para deixar o programa rodando.
+    text = input("Please insert a name to search. ")
+    text = text.split(' ')          #Separa o texto em diferentes partes.
+    if (text[0] == 'Exit'):         #Exit para terminar o programa.
+        print("Ending program.")
+        running = 0             
+    elif (text[0] == 'player'):     #Caso digite player, procura o jogador solicitado.                                           
+        if (len(text) >= 2):
+            del text[0]             #Deleta a parte "player", da string.
+            text = transform_to_str(text)        #Transforma o vetor de volta em uma string.   
+            consult = (tr.search(text))          #Pega todos os nomes, dado o prefixo dado   
+            print("\nsofifa_id   name              player_positions   rating   count")
+            for x in consult:                                        #Para cada nome encontrado.
+                info = realizar_consulta_players(players_vet,x)      #Recebe o resto das informações pela tabela hash dos jogadores.    
+                print(info)                                          #Printa as informações adicionais.
+        else:
+            print("Please, insert a name after player")    #Caso o usuário insira somente "player".            
+    else:
+        print("Wrong command. Please, try again. ")        #Caso o usuário insira algum comando inválido. 
 #--------------------------------------------------------
 # 
 #   O que a gente tem de lista e array
@@ -271,6 +256,4 @@ for x in consult:                   #Para cada nome encontrado.
 #   players_media = ID, rating, rating, rating...
 # 
 # #
-#
-# 
-# ----------
+#----------
